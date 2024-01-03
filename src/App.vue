ec5e72b0e1ea4709c15b7aaa7f3ca9e7ad86d280
MatheusLadeiras <69425532+MatheusLadeiras@users.noreply.github.com>
2024-01-03 01:18:07 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-    <TheHeader v-if="showHeader"/>
-    <div v-show="showName">
-      Nome:      {{ firstName }} <br>
-      Sobrenome: {{ lastName }}
-    </div>
-    <div v-if="acessLevel === 'admin'">Acesso Admin</div>
-    <div v-else-if="acessLevel === 'marketing'">Acesso Marketing</div>
-    <div v-else>User</div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <!--<TwoWayDataBindingVue />-->
-    <Events />
-    <Computed />
+  <TheHeader v-if="showHeader"/>
+  <div v-show="showName">
+    Nome:      {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+  <div v-if="acessLevel === 'admin'">Acesso Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Acesso Marketing</div>
+  <div v-else>User</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <!--<TwoWayDataBindingVue />-->
+  <Events />
+  <Computed />
+  <Observers />
 </template> 
 
 <script>
@@ -19,37 +20,39 @@
 import TheHeader from './components/TheHeader';
 //import TwoWayDataBindingVue from './components/TwoWayDataBinding';
 import Events from './components/Events';
-import Computed from './components/Computed'
+import Computed from './components/Computed';
+import Observers from './components/Observers';
 
 export default {
-  name: 'App',
-  aaa: false,
-  components: {
-    //HelloWorld,
-    TheHeader,
-    //TwoWayDataBindingVue,
-    Events,
-    Computed,
-  },
-  data() {
-    return {
-      showHeader: false, //Choose in "true" to show the "TheHeader"
-      firstName: 'Jon',
-      lastName: 'Snow',
-      showName: false,
-      acessLevel: 'marketing',
-    }
+name: 'App',
+aaa: false,
+components: {
+  //HelloWorld,
+  TheHeader,
+  //TwoWayDataBindingVue,
+  Events,
+  Computed,
+  Observers
+},
+data() {
+  return {
+    showHeader: false, //Choose in "true" to show the "TheHeader"
+    firstName: 'Jon',
+    lastName: 'Snow',
+    showName: false,
+    acessLevel: 'marketing',
   }
+}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 60px;
 }
 </style>
