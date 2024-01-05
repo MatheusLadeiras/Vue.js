216a014ae2877aa36ff5abe95fd11dcbb219f2d9
MatheusLadeiras <69425532+MatheusLadeiras@users.noreply.github.com>
2024-01-05 11:34:43 -0300

    Create BaseAlert.vue

---
+++ src/components/BaseAlert.vue
@@ -0,0 +1,28 @@
+<template>
+    <br><br><br>
+    <div>
+        - Base Alert -<br>
+        ========================================<br><br>
+        <div :class="['alert', {'alert-success': variant === 'success'}]">Seu formulário foi enviado com sucesso</div><br>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['variant'],
+    }
+</script>
+
+
+<style scoped>
+    .alert {
+        padding: 17px;
+        border-radius: 10px;
+        color: rgb(255, 230, 0);
+        background: red;
+    }
+    .alert-success {
+        background: rgb(72, 255, 0);
+        color: #fff;
+    }
+</style>
