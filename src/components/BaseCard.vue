815246307683ddb445da87221a60acf009c13287
MatheusLadeiras <69425532+MatheusLadeiras@users.noreply.github.com>
2024-01-05 10:47:01 -0300

    Create BaseCard.vue

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <br><br><br>
+    <div>
+        - Base Card -<br>
+        ========================================<br><br>
+        <div class="card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing</div>
+    </div>
+</template>
+
+
+<style scoped>
+    .card {
+        background: darkcyan;
+        padding: 10px;
+        border-radius: 10px;
+    }
+</style>
